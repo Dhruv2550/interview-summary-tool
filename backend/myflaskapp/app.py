1b4f0cf40a06cb59a0e437799563d2d304666c95
Dhruv2550 <dhruvtreddy@gmail.com>
2025-05-29 14:22:59 -0400

    trying again

--- backend/myflaskapp/app.py
+++ backend/myflaskapp/app.py
@@ -26,8 +26,8 @@ CORS(app, origins=ALLOWED_ORIGINS)
 
 # Use environment variable for database URL
 # url has to be in the format: postgresql+psycopg2://username:password@host/database_name
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 print(">> DATABASE_URL from env:", os.getenv("DATABASE_URL"))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 
